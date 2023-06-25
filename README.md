81e1dd0a29eaeba81afc3ab58d9ef9b62a81e1a5
Cedric Stark <cedric.stark@outlook.com>
2023-06-25 23:18:05 +0200

    chore(src): Add Google demo and make it runnable

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# drawmatic
+# Drawmatic
+
+## Overview
+
+Draw images using nothing but your webcam and fingers!
+
+## Link
+
+Use [this link](http://127.0.0.1:5500/drawmatic/dist/index.html?model=mediapipe_hands) to access the locally deployed page.
+
+## TODO
+
+1. Draw path of index finger
+1. Add buttons to export image
+1. Only draw when index and middle finger raised together
+1. Do some design stuff
+
+https://github.com/tensorflow/tfjs-models/tree/master/hand-pose-detection
---
+++ src/index.js
@@ -0,0 +1,174 @@
+import '@tensorflow/tfjs-backend-webgl';
+import * as mpHands from '@mediapipe/hands';
+
+import * as tfjsWasm from '@tensorflow/tfjs-backend-wasm';
+
+import * as handdetection from '@tensorflow-models/hand-pose-detection';
+
+import { Camera } from './camera';
+import { setupDatGui } from './option_panel';
+import { STATE } from './util/params';
+import { setupStats } from './util/stats_panel';
+import { setBackendAndEnvFlags } from './util/util';
+
+tfjsWasm.setWasmPaths(
+  `https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-wasm@${tfjsWasm.version_wasm}/dist/`
+);
+
+let detector, camera, stats;
+let startInferenceTime,
+  numInferences = 0;
+let inferenceTimeSum = 0,
+  lastPanelUpdate = 0;
+let rafId;
+
+async function createDetector() {
+  switch (STATE.model) {
+    case handdetection.SupportedModels.MediaPipeHands:
+      const runtime = STATE.backend.split('-')[0];
+      if (runtime === 'mediapipe') {
+        return handdetection.createDetector(STATE.model, {
+          runtime,
+          modelType: STATE.modelConfig.type,
+          maxHands: STATE.modelConfig.maxNumHands,
+          solutionPath: `https://cdn.jsdelivr.net/npm/@mediapipe/hands@${mpHands.VERSION}`,
+        });
+      } else if (runtime === 'tfjs') {
+        return handdetection.createDetector(STATE.model, {
+          runtime,
+          modelType: STATE.modelConfig.type,
+          maxHands: STATE.modelConfig.maxNumHands,
+        });
+      }
+  }
+}
+
+async function checkGuiUpdate() {
+  if (STATE.isTargetFPSChanged || STATE.isSizeOptionChanged) {
+    camera = await Camera.setupCamera(STATE.camera);
+    STATE.isTargetFPSChanged = false;
+    STATE.isSizeOptionChanged = false;
+  }
+
+  if (STATE.isModelChanged || STATE.isFlagChanged || STATE.isBackendChanged) {
+    STATE.isModelChanged = true;
+
+    window.cancelAnimationFrame(rafId);
+
+    if (detector != null) {
+      detector.dispose();
+    }
+
+    if (STATE.isFlagChanged || STATE.isBackendChanged) {
+      await setBackendAndEnvFlags(STATE.flags, STATE.backend);
+    }
+
+    try {
+      detector = await createDetector(STATE.model);
+    } catch (error) {
+      detector = null;
+      alert(error);
+    }
+
+    STATE.isFlagChanged = false;
+    STATE.isBackendChanged = false;
+    STATE.isModelChanged = false;
+  }
+}
+
+function beginEstimateHandsStats() {
+  startInferenceTime = (performance || Date).now();
+}
+
+function endEstimateHandsStats() {
+  const endInferenceTime = (performance || Date).now();
+  inferenceTimeSum += endInferenceTime - startInferenceTime;
+  ++numInferences;
+
+  const panelUpdateMilliseconds = 1000;
+  if (endInferenceTime - lastPanelUpdate >= panelUpdateMilliseconds) {
+    const averageInferenceTime = inferenceTimeSum / numInferences;
+    inferenceTimeSum = 0;
+    numInferences = 0;
+    stats.customFpsPanel.update(
+      1000.0 / averageInferenceTime,
+      120 /* maxValue */
+    );
+    lastPanelUpdate = endInferenceTime;
+  }
+}
+
+async function renderResult() {
+  if (camera.video.readyState < 2) {
+    await new Promise((resolve) => {
+      camera.video.onloadeddata = () => {
+        resolve(video);
+      };
+    });
+  }
+
+  let hands = null;
+
+  // Detector can be null if initialization failed (for example when loading
+  // from a URL that does not exist).
+  if (detector != null) {
+    // FPS only counts the time it takes to finish estimateHands.
+    beginEstimateHandsStats();
+
+    // Detectors can throw errors, for example when using custom URLs that
+    // contain a model that doesn't provide the expected output.
+    try {
+      hands = await detector.estimateHands(camera.video, {
+        flipHorizontal: false,
+      });
+    } catch (error) {
+      detector.dispose();
+      detector = null;
+      alert(error);
+    }
+
+    endEstimateHandsStats();
+  }
+
+  camera.drawCtx();
+
+  // The null check makes sure the UI is not in the middle of changing to a
+  // different model. If during model change, the result is from an old model,
+  // which shouldn't be rendered.
+  if (hands && hands.length > 0 && !STATE.isModelChanged) {
+    camera.drawResults(hands);
+  }
+}
+
+async function renderPrediction() {
+  await checkGuiUpdate();
+
+  if (!STATE.isModelChanged) {
+    await renderResult();
+  }
+
+  rafId = requestAnimationFrame(renderPrediction);
+}
+
+async function app() {
+  // Gui content will change depending on which model is in the query string.
+  const urlParams = new URLSearchParams(window.location.search);
+  if (!urlParams.has('model')) {
+    alert('Cannot find model in the query string.');
+    return;
+  }
+
+  await setupDatGui(urlParams);
+
+  stats = setupStats();
+
+  camera = await Camera.setupCamera(STATE.camera);
+
+  await setBackendAndEnvFlags(STATE.flags, STATE.backend);
+
+  detector = await createDetector();
+
+  renderPrediction();
+}
+
+app();
